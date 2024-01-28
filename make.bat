369f235d96eaa4090eff25bd2aad5c3956bcebce
Mike Mclain <mamclain@gmail.com>
2024-01-28 07:15:50 -0500

    fix some problems with run

--- make.bat
+++ make.bat
@@ -23,14 +23,49 @@
 @REM use enabledelayedexpansion to allow for the use of ! in variables
 setlocal enabledelayedexpansion
 
+@REM check for docker (exit if not found)
+call :CheckForDocker
 
 @REM Load a .env file into the current environment
+echo "Loading .env file..."
 call :LoadEnvFile .env
 
 @REM Check if the environmental variables exist, if not set them to the default values
+echo "evaluating build env vars..."
 call :CheckOrSetEnvVar REGISTRY "localhost"
 call :CheckOrSetEnvVar PROJECT "sknnr"
 call :CheckOrSetEnvVar IMAGE "enshrouded-server-testing"
+
+echo "evaluating run env vars..."
+@REM provide the user some configuration options for the container run
+call :CheckOrSetEnvVar CONTAINER_NAME "enshrouded-test"
+call :CheckOrSetEnvVar FILE_MOUNT "./temp:/home/steam/enshrouded/savegame"
+call :CheckOrSetEnvVar SERVER_NAME "Enshrouded Containerized Server"
+call :CheckOrSetEnvVar SERVER_SLOTS "16"
+call :CheckOrSetEnvVar SERVER_PASSWORD "ChangeThisPlease"
+
+
+
+@REM set the default run options...
+set DEFAULT_RUN_OPTS=
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --name '!CONTAINER_NAME!'
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --rm
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! -it
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! -d
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! -v '!FILE_MOUNT!'
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! -p 15636:15636/udp
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! -p 15637:15637/udp
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --env SERVER_NAME='!SERVER_NAME!'
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --env SERVER_SLOTS=!SERVER_SLOTS!
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --env SERVER_PASSWORD='!SERVER_PASSWORD!'
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --env GAME_PORT=15636
+set DEFAULT_RUN_OPTS=!DEFAULT_RUN_OPTS! --env QUERY_PORT=15637
+
+@REM echo Default Run Options: !DEFAULT_RUN_OPTS!
+@REM allow the user to override the default run options fully or partially
+call :CheckOrSetEnvVar RUN_OPTS "!DEFAULT_RUN_OPTS!" replace
+
+@REM echo Run Options: !RUN_OPTS!
 
 @REM set the image tag based on the TAG environmental variable or the git commit hash otherwise
 call :SetImageTag
@@ -45,6 +80,11 @@ if /i "%1%"=="full" (
 ) else if /i "%1%"=="push" (
     echo Push the project...
     docker push %IMAGE_REF%
+) else if /i "%1%"=="run" (
+    echo Run the project...
+    call :CheckForImage !IMAGE_REF!
+    echo docker run !RUN_OPTS! !IMAGE_REF!
+    docker run !RUN_OPTS! !IMAGE_REF!
 ) else (
     echo Building the project...
     docker build --progress=plain -f ./container/Containerfile -t %IMAGE_REF% ./container/
@@ -55,7 +95,7 @@ goto :eof
 
 @REM set the docker image path
 :SetImagePath
-IF "%REGISTRY%"=="" (
+IF %REGISTRY%=="" (
     echo %PROJECT%/%IMAGE%:%TAG%
     @REM this is a docker hub image so drop the registry prefix
     set IMAGE_REF=%PROJECT%/%IMAGE%:%TAG%
@@ -89,7 +129,11 @@ goto :eof
 IF EXIST %~1 (
     for /F "tokens=1* delims==" %%a in (%~1) do (
         echo %%a=%%b
-        set %%a=%%b
+        IF "%%b"=="" (
+            set %%a=""
+        ) ELSE (
+            set %%a=%%b
+        )
     )
 ) ELSE (
     echo "%~1 file does not exist."
@@ -98,13 +142,36 @@ goto :eof
 
 @REM Check if the environmental variables exist, if not set them to the default values
 :CheckOrSetEnvVar
-IF "!%~1!"=="" (
-    echo Environmental %~1 exists with value: !%~1%!
-) ELSE IF NOT DEFINED %~1 (
+echo Checking for environmental %~1
+IF NOT DEFINED %~1 (
     echo Environmental %~1 does not exist, setting it as local now of: %~2
     set %~1=%~2
+    IF "%~3"=="replace" (
+        set "%~1=!%~1:'="!"
+    )
 ) ELSE (
     echo Environmental %~1 exists with value: !%~1%!
+)
+goto :eof
+
+
+:CheckForDocker
+docker --version >nul 2>&1
+if errorlevel 1 (
+    echo Docker is not installed. Exiting.
+    exit /b
+) else (
+    echo Docker is installed.
+)
+goto :eof
+
+:CheckForImage
+docker images %~1  | findstr /i "\!%~1!\>" >nul
+if errorlevel 1 (
+    echo Image does not exist. Exiting.
+    exit /b
+) else (
+    echo Image exists.
 )
 goto :eof
 
